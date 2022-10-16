5deea04e54086ac05feeafe5e690257a4ac8852a
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-10-16 22:26:06 +0200

    Flattened arrays instead of making getters

--- fplot.py
+++ fplot.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 
-idx_ref = lambda val, tup: val if len(tup) == 0 else idx_ref(val[tup[0]], tup[1:])
-flat_n = lambda lsts, n: lsts if n == 0 else flat_n([e for sl in lsts for e in sl], n-1)
-
-# should be replaced, by more general solution
-def dim_range(*shape):
-    nr, nc, *r = [*shape, None, None, None] # eww
-    if not nr: return [[]]                     # case 1
-    if not nc: return [[i] for i in range(nr)] # case 2... so on
-    return [(i,j) for i in range(nr) for j in range(nc)]
-
+flat_n = lambda lsts, n: lsts if n <= 1 else flat_n([e for sl in lsts for e in sl], n-1)
 get_shape = lambda lst: (len(lst), *get_shape(lst[0])) if isinstance(lst, list) else ()
 
-def subplots(t, val, axs, dims, args):
-    for idx in dim_range(*dims):
-        if t == "img": idx_ref(axs, idx).imshow ( idx_ref(val, idx), **args)
-        else: getattr(idx_ref(axs, idx), t)(*idx_ref(val, idx), **args)
+def subplots(t, val, axs, args):
+    for ax, v in zip(axs, val):
+        if t == "img": ax.imshow (v, **args)
+        else: getattr(ax, t)(*v, **args)
 
 def stack(*layers):
-    shape    = get_shape(list(layers[0].values())[0])[:-2]
-    # Maybe the axs getter can be replaced by flattening the input and axs
-    fig, axs = plt.subplots(*shape)
+    shape = get_shape(list(layers[0].values())[0])[:-2]
+    axs_  = plt.subplots(*shape)[1]
+    print(len(shape), axs_[0])
+    axs   = flat_n(axs_, len(shape))
     for l in layers:
-        plot = [[k,v] for k,v in l.items() if k     in ["plot", "img", "scatter", "bar"]]
-        args = [[k,v] for k,v in l.items() if k not in ["plot", "img", "scatter", "bar"]]
-        subplots(*plot[0], axs, shape, dict(args))
+        t, val = [[k,v] for k,v in l.items() if k     in ["plot", "img", "scatter", "bar"]][0]
+        args   = [[k,v] for k,v in l.items() if k not in ["plot", "img", "scatter", "bar"]]
+        subplots(t, flat_n(val, len(shape)), axs, dict(args))
 
 def fplot(*args, style_=None):
     stack(*args)
