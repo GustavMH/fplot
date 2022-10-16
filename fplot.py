6311572ef58a864cc9b42a5ce0024539e3fafe1d
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-10-16 15:56:12 +0200

    Fixed inconcistancy

--- fplot.py
+++ fplot.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 
 idx_ref = lambda val, tup: val if len(tup) == 0 else idx_ref(val[tup[0]], tup[1:])
 
+# should be replaced, by more general solution
 def dim_range(*shape):
-    nr, nc, *r = [*shape, None, None, None]
-    if not nr: return [[]]
-    if not nc: return [[i] for i in range(nr)]
+    nr, nc, *r = [*shape, None, None, None] # eww
+    if not nr: return [[]]                     # case 1
+    if not nc: return [[i] for i in range(nr)] # case 2... so on
     return [(i,j) for i in range(nr) for j in range(nc)]
 
 get_shape = lambda lst: (len(lst), *get_shape(lst[0])) if isinstance(lst, list) else ()
@@ -17,17 +18,17 @@ def split_dict(d, keys):
 
 def subplots(t, val, axs, dims, args):
     for idx in dim_range(*dims):
-        if t == "line"   : idx_ref(axs, idx).plot   (*idx_ref(val, idx), **args)
+        if t == "plot"   : idx_ref(axs, idx).plot   (*idx_ref(val, idx), **args)
         if t == "img"    : idx_ref(axs, idx).imshow ( idx_ref(val, idx), **args)
         if t == "scatter": idx_ref(axs, idx).scatter(*idx_ref(val, idx), **args)
         if t == "bar"    : idx_ref(axs, idx).bar    (*idx_ref(val, idx), **args)
 
 def stack(*layers):
-    shape    = get_shape(list(layers[0].values())[0])
-    fig, axs = plt.subplots(*[1,*shape[:len(shape)-2]][-2:])
+    shape    = get_shape(list(layers[0].values())[0])[:len(shape)-2]
+    fig, axs = plt.subplots(*[1,*shape][-2:])
     for l in layers:
         t, val, args = split_dict(l, ["plot", "img", "scatter", "bar"])
-        subplots(t, val, axs, shape[:len(shape)-2], args)
+        subplots(t, val, axs, shape, args)
 
 def fplot(*args, style_=None):
     stack(*args)
