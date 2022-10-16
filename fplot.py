73d3103806e08a08593642d25d18213194fc4134
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-10-16 16:06:06 +0200

    Added comments

--- fplot.py
+++ fplot.py
@@ -12,10 +12,12 @@ def dim_range(*shape):
 
 get_shape = lambda lst: (len(lst), *get_shape(lst[0])) if isinstance(lst, list) else ()
 
+# should also be replaced, dont know how
 def split_dict(d, keys):
     return [*[[k,v] for k,v in d.items() if k in keys][0],
             dict([[k,v] for k,v in d.items() if k not in keys])]
 
+# getattr should remove the if chunk
 def subplots(t, val, axs, dims, args):
     for idx in dim_range(*dims):
         if t == "plot"   : idx_ref(axs, idx).plot   (*idx_ref(val, idx), **args)
