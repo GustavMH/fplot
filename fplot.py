2ab6ba8dc83eed34197bcc5592ef4f2470ab2d9f
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-10-16 22:11:49 +0200

    removed split_dict

--- fplot.py
+++ fplot.py
@@ -2,6 +2,7 @@
 import matplotlib.pyplot as plt
 
 idx_ref = lambda val, tup: val if len(tup) == 0 else idx_ref(val[tup[0]], tup[1:])
+flat_n = lambda lsts, n: lsts if n == 0 else flat_n([e for sl in lsts for e in sl], n-1)
 
 # should be replaced, by more general solution
 def dim_range(*shape):
@@ -12,29 +13,23 @@ def dim_range(*shape):
 
 get_shape = lambda lst: (len(lst), *get_shape(lst[0])) if isinstance(lst, list) else ()
 
-# should also be replaced, dont know how
-def split_dict(d, keys):
-    return [*[[k,v] for k,v in d.items() if k in keys][0],
-            dict([[k,v] for k,v in d.items() if k not in keys])]
-
 def subplots(t, val, axs, dims, args):
     for idx in dim_range(*dims):
         if t == "img": idx_ref(axs, idx).imshow ( idx_ref(val, idx), **args)
         else: getattr(idx_ref(axs, idx), t)(*idx_ref(val, idx), **args)
 
 def stack(*layers):
-    shape    = get_shape(list(layers[0].values())[0])
+    shape    = get_shape(list(layers[0].values())[0])[:-2]
     # Maybe the axs getter can be replaced by flattening the input and axs
-    fig, axs = plt.subplots(*shape[:-2])
+    fig, axs = plt.subplots(*shape)
     for l in layers:
-        t, val, args = split_dict(l, ["plot", "img", "scatter", "bar"])
-        subplots(t, val, axs, shape[:-2], args)
+        plot = [[k,v] for k,v in l.items() if k     in ["plot", "img", "scatter", "bar"]]
+        args = [[k,v] for k,v in l.items() if k not in ["plot", "img", "scatter", "bar"]]
+        subplots(*plot[0], axs, shape, dict(args))
 
 def fplot(*args, style_=None):
     stack(*args)
     style = style_ if style_ else {}
     for k, v in style.items():
-        {"title":  plt.title,
-         "ylabel": plt.ylabel,
-         "xlabel": plt.xlabel,
-         "legend": plt.legend}[k](v)
+        {"title":  plt.title,  "ylabel": plt.ylabel,
+         "xlabel": plt.xlabel, "legend": plt.legend}[k](v)
