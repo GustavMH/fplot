bbca58986c996e68466462a2b4a11bbbbca7ea84
GustavMH <45123074+GustavMH@users.noreply.github.com>
2022-10-17 07:53:11 +0200

    Added example and removed keys limits for the plot

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 A wrapper for matplotlib that manages plot mutation
+
+``` python
+from fplot import fplot
+fplot(["scatter", [[[1,2,3],[3,2,1]],[[1,2,3],[3,2,1]]], 
+      {"c": "red"}], style={"title": "Hej"})
+```
+
--- fplot.py
+++ fplot.py
@@ -4,18 +4,16 @@ import matplotlib.pyplot as plt
 flat_n = lambda lsts, n: lsts if n <= 1 else flat_n([e for sl in lsts for e in sl], n-1)
 get_shape = lambda lst: (len(lst), *get_shape(lst[0])) if isinstance(lst, list) else ()
 
-def subplots(t, val, axs, args):
+def subplots(t, val, axs, args={}):
     for ax, v in zip(axs, val):
         if t == "img": ax.imshow (v, **args)
         else: getattr(ax, t)(*v, **args)
 
 def stack(*layers):
-    shape = get_shape(list(layers[0].values())[0])[:-2]
+    shape = get_shape(layers[0][1])[:-2]
     axs   = flat_n(plt.subplots(*shape)[1], len(shape))
-    for l in layers:
-        t, val = [[k,v] for k,v in l.items() if k     in ["plot", "img", "scatter", "bar"]][0]
-        args   = [[k,v] for k,v in l.items() if k not in ["plot", "img", "scatter", "bar"]]
-        subplots(t, flat_n(val, len(shape)), axs, dict(args))
+    for t, val, *args in layers:
+        subplots(t, flat_n(val, len(shape)), axs, *args)
 
 def fplot(*args, style={}):
     stack(*args)
